bf4a4a0cd54c8366c17ef0ea49bc95456a0cb9bf
sifathabib2 <sifathabib34@gmail.com>
2024-09-19 10:11:53 +0600

    2nd commit

--- js/login.js
+++ js/login.js
@@ -7,11 +7,21 @@
 document.getElementById('btn-login').addEventListener('click', function (event) {
     // step2 : prevent default behavior (prevent reloading browser) 
     event.preventDefault();     // vejal for beginners
-    console.log('login button clicked');
-    // step3 : get the phone number
+    // console.log('login button clicked');
+    // step3 : get the phone number and pin
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-    
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+    // step 4: validate phone and pin
+    // this is temporary. You shouldn't do like this'
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        console.log('wrong phone number or pin');
+        
+    }
 
 
 })
